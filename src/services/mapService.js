9579e141841a62c7fff213d82a7f871a954f5524
rafadavesac <rafalaimerdavesac@gmail.com>
2026-06-18 22:54:59 -0300

    adapta mapService para campos do MedHome

--- src/services/mapService.js
+++ src/services/mapService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://two025-01-apisample.onrender.com/ws/point'; //TODO: colocar minha propria api construida em java
+const BASE_URL = 'http://localhost:8080/ws/point';
 
 export async function getPoints(token) {
   try {
@@ -10,54 +10,16 @@ export async function getPoints(token) {
       },
     });
 
-    // Mocked response
-    /*
-    const response = {
-      status: 200,
-      data: [
-      {
-        id: 1,
-        descricao: 'Avenida Paulista',
-        latitude: -23.561684,
-        longitude: -46.656139,
-      },
-      {
-        id: 2,
-        descricao: 'Parque Ibirapuera',
-        latitude: -23.587416,
-        longitude: -46.657634,
-      },
-      {
-        id: 3,
-        descricao: 'Mercadão Municipal',
-        latitude: -23.541212,
-        longitude: -46.627684,
-      },
-      {
-        id: 4,
-        descricao: 'Estação da Luz',
-        latitude: -23.536578,
-        longitude: -46.633309,
-      },
-      ],
-    };
-    */
-
-    // o objeto response.data possui os campos latitude e longitude mas precisamos mudar os nomes para lat lng
     const points = response.data.map(point => ({
       id: point.id,
-      title: point.descricao,
+      title: point.patientName,
       position: {
         lat: point.latitude,
         lng: point.longitude,
       },
     }));
 
-    if (response.status === 200) {
-      return points;
-    } else {
-      throw new Error('Erro ao buscar pontos');
-    }
+    return points;
   } catch (error) {
     throw new Error(error.response?.data?.message || 'Erro ao buscar pontos');
   }
@@ -71,22 +33,12 @@ export async function postPoint(token, pointData) {
       },
     });
 
-    // Mocked response
-    /*
-    const response = {
-      status: 200,
-      data: {
-      id: Math.floor(Math.random() * 10000),
-      ...pointData,
-      },
-    };
-    */
     if (response.status === 201) {
       return response.data;
     } else {
-      throw new Error('Erro ao cadastrar ponto');
+      throw new Error('Erro ao cadastrar atendimento');
     }
   } catch (error) {
-    throw new Error(error.response?.data?.message || 'Erro ao cadastrar ponto');
+    throw new Error(error.response?.data?.message || 'Erro ao cadastrar atendimento');
   }
 }
